bab0b32b42d7070fe34520490b2039767100f06a
faitz <faitz@pm.me>
2020-10-08 12:30:28 +0200

    remove noborder patch & change vanitygap config to enable outer border

--- config.def.h
+++ config.def.h
@@ -9,7 +9,7 @@ static const unsigned int gappih    = 25;       /* horiz inner gap between windo
 static const unsigned int gappiv    = 25;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 25;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 25;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
 static const char dmenufont[]       = "monospace:size=10";
