bda57eca6db7f06ae1a36003408f161af5963883
faitz <faitz@pm.me>
2020-10-11 20:19:37 +0200

    update emoji bar support

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static const unsigned int gappoh    = 25;       /* horiz outer gap between windo
 static const unsigned int gappov    = 25;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
-static const char *fonts[]          = { "monospace:size=10", "-misc-font JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
+static const char *fonts[]          = {"monospace:size=10", "JoyPixels:pixelsize=14:antialias=true:autohint=true"};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
