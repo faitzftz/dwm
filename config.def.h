f4cbd8cc8603e39925171a7bba07f2296f0c6fbc
faitz <faitz@pm.me>
2020-10-26 21:57:59 +0100

    commit changes to def.h

--- config.def.h
+++ config.def.h
@@ -72,7 +72,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 #include "movestack.c"
 
@@ -121,8 +121,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Tab,    cyclelayout,    {.i = +1 } },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	/*{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } }, */
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
@@ -138,7 +138,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|Mod4Mask,              XK_q,      quit,           {0} },
 	{ MODKEY|Mod4Mask,              XK_r,      quit,           {1} },
-	{ MODKEY|Mod4Mask,              XK_o,      spawn,          SHCMD("/usr/bin/slimlock") },
+	{ MODKEY|Mod4Mask,              XK_o,      spawn,          SHCMD("slock") },
 	{ MODKEY|Mod4Mask,              XK_p,      spawn,          SHCMD("systemctl suspend") },
         { MODKEY|ShiftMask,             XK_e,      spawn,          SHCMD("$HOME/.local/bin/dmenuunicode") },
 };
