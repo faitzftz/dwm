e4645b7d87d06db57c4ab93bfbde024cef6a0902
faitz <faitz@pm.me>
2021-01-01 21:39:43 +0100

    window ordering

--- config.def.h
+++ config.def.h
@@ -38,6 +38,10 @@ static const Rule rules[] = {
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ NULL,     "protonmail-bridge", NULL, 1 << 8,	  0,	      0,          -1,        -1 },
+	{ "Signal", NULL,      NULL,	       1 << 2,    0,          0,          -1,       -1},
+	{ "Thunderbird", NULL, NULL,	       1 << 1,	  0,	      0,	  -1,	     -1 },
+	{ "java", NULL, "Cryptomator",  1 << 8,	  0,	      0,	  -1,	     -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
